40dc1d7fb2a83a7bb0ad798d037cb966c19da0dc
Justin Ang <justinang177@gmail.com>
2025-01-02 15:45:18 -0800

    update nvim to catpucchin-mocha

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+  -- add
+  { "catppuccin/nvim" },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin-mocha",
+    },
+  },
+}
